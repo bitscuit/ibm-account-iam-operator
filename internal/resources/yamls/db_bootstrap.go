58dca8bdec00013d26f7c29a80a3c7361954eb42
Henry H Li <henry.h.li@ibm.com>
2024-07-12 16:11:02 -0400

    added database initialization job
    Signed-off-by: Henry H Li <henry.h.li@ibm.com>

---
+++ internal/resources/yamls/db_bootstrap.go
@@ -0,0 +1,51 @@
+package yamls
+
+const DB_BOOTSTRAP_JOB = `
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: create-account-iam-db
+spec:
+  template:
+    metadata:
+      name: create-account-iam-db
+    spec:
+      containers:
+      - name: postgres
+        image: docker-na-public.artifactory.swg-devops.com/hyc-cloud-private-integration-docker-local/ibmcom/mcsp-utils:latest
+        command: ["/bin/bash", "/db-init/create_db.sh"]
+        volumeMounts:
+        - name: script-volume
+          mountPath: /scripts
+        - name: psql-credentials
+          mountPath: /psql-credentials
+        - name: db-password
+          mountPath: /db-password
+        - name: data-volume
+          mountPath: /data
+      restartPolicy: OnFailure
+      volumes:
+      - name: script-volume
+        configMap:
+          name: create-account-iam-db
+      - name: psql-credentials
+        secret:
+          secretName: common-service-db-superuser
+          items:
+          - key: username
+            path: username
+          - key: password
+            path: password
+          defaultMode: 420  
+      - name: db-password
+        secret:
+          secretName: account-im-db-password
+          items:
+          - key: password
+            path: password
+          defaultMode: 420  
+      - name: data-volume
+        emptyDir: {}      
+  backoffLimit: 4
+
+`
