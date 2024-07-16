4e15a4e485215bb28e3c15fdba856133ca92f0d5
Henry H Li <henry.h.li@ibm.com>
2024-07-16 10:10:24 -0400

    added mcsp-im-config job manifests, no logic yet
    Signed-off-by: Henry H Li <henry.h.li@ibm.com>

--- internal/resources/yamls/utils_job_rbac.go
+++ internal/resources/yamls/utils_job_rbac.go
@@ -1,13 +1,48 @@
 package yamls
 
 const UTILS_JOB_ROLE = `
-
+kind: Role
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: mcsp-utils
+rules:
+  - verbs:
+      - get
+      - list
+      - watch
+    apiGroups:
+      - ''
+    resources:
+      - secrets
+	  - configmaps
+	  - pods
+  - verbs:
+      - create
+      - update
+      - delete
+    apiGroups:
+      - ''
+    resources:
+      - secrets
 `
 
 const UTILS_JOB_SA = `
-
+kind: ServiceAccount
+apiVersion: v1
+metadata:
+  name: mcsp-utils
 `
 
 const UTILS_JOB_RB = `
-
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: mcsp-utils
+subjects:
+  - kind: ServiceAccount
+    name: mcsp-utils
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: mcsp-utils
 `
