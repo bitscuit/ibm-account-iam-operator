bbfaaefe5d517abe29e3cd0ade7257a5834905e2
Henry H Li <henry.h.li@ibm.com>
2024-07-23 18:05:57 -0400

    chech go bin
    Signed-off-by: Henry H Li <henry.h.li@ibm.com>

--- make/ibm.mk
+++ make/ibm.mk
@@ -113,7 +113,7 @@ config-docker: get-cluster-credentials
 check: ## @code Run the code check
 	which go
 	echo $(PATH)
-	ls /usr/local/go
+	ls /usr/local/go/bin
 	ls /usr/local/go/pkg/tool
 	ls /usr/local/go/pkg/tool/linux_amd64
 	go fmt ./...
